29f438ad0b4fdfb08542431a12edd407843e5bb4
sunxiaolei <xiaoleisun92@gmail.com>
2019-01-16 10:43:18 +0800

    test

--- lib/flutter_plugin_test.dart
+++ lib/flutter_plugin_test.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class FlutterPluginTest {
   static const MethodChannel _channel =
@@ -9,5 +10,13 @@ class FlutterPluginTest {
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static showToast(msg) {
+    Fluttertoast.showToast(
+        msg: msg,
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIos: 1);
   }
 }
